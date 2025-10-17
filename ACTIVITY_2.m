ee40678a56462eb537f6ac7cb8c923958df8e849
Bihan Lakshitha Siriwardana <137089511+BLsiriwardana@users.noreply.github.com>
2025-10-16 18:33:25 -0700

    Activity_2
    solve some mathematical expressions

---
+++ ACTIVITY_2.m
@@ -0,0 +1,27 @@
+% Write a MATLAB program to calculate the following expression and round the 
+% answers to the nearest integer.  
+
+% Q1. 
+
+x1 = 2; % creating x1, and y1 variable as x,y.
+y1 = 4;
+
+z1 = round(sqrt(5*x1^2 + y1^2))
+
+% Q2.
+
+x2 =  pi/4;
+
+z2 = round(4*cos(x2) + j*6*sin(x2))
+
+% Q3.
+
+x3 = pi/3;
+y2 = 2;
+
+z3 = round(3*sin(x3) + 4*cos(x3) + 3*exp(y2))
+
+% Q4.
+
+x = pi/2;
+y = sin(x) / x;
