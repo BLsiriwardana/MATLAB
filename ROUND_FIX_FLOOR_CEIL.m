ceb6aea5228906236dbbd24425f105688a2dd1fc
Bihan Lakshitha Siriwardana <137089511+BLsiriwardana@users.noreply.github.com>
2025-10-24 10:01:28 -0700

    Create ROUND_FIX_FLOOR_CEIL.m

---
+++ ROUND_FIX_FLOOR_CEIL.m
@@ -0,0 +1,47 @@
+% usage of [round, fix, celi, floor] commands
+
+% 1.3 to ----> 1
+
+fix(1.3);
+
+round(1.3);
+
+floor(1.3);
+
+% 1.5 to ----> 1
+
+fix(1.5);
+
+floor(1.5);
+
+% 1.9 to ----> 2
+
+round(1.9);
+
+ceil(1.9);
+
+% 11.9 to ----> 11
+
+floor(11.9);
+
+fix(11.9);
+
+% -2.9 to ----> -2
+
+fix(-2.9);
+
+ceil(-2.9);
+
+% -3.9 to ----> -4
+
+round(-3.9);
+
+floor(-3.9);
+
+% 3.4 to ----> 3
+
+round(3.4)
+
+floor(3.4)
+
+fix(3.4)
